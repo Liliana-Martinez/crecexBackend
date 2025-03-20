7b39941856db6c08534bafe5e3455cb584f19e15
Liliana-Martinez <lilia001125@gmail.com>
2025-03-20 04:51:39 -0600

    Se creo server

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//Conexión a la bd usando las variables de .env
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+db.connect(err => {
+    if(err) {
+        console.error('Error conectando la BD:', err)
+        return;
+    }
+    console.log('Conectado a la base de datos');
+});
+
+app.listen(process.env.PORT, () => console.log(`Servidor corriendo en el puerto ${process.env.PORT}`));
+/*app.get('/', (req, res) => {
+    res.send('¡Backend funcionando!');
+});*/
+
